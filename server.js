b25766029bb4112ebc2cbce042be0d0cc3f30b56
Abhilash <abhilash.kulkarni@ymedialabs.com>
2018-03-06 12:22:08 +0530

    pick compressed file in production
    pick compressed file in production

--- server.js
+++ server.js
@@ -1,13 +1,28 @@
 var express = require('express'),
    path = require('path'),
+   compression = require('compression'),
    config = require('./config/config.js');
 
 
 var app = express();
 
-
 /* directory use */
 var cachetime = 1000 * 3600 * 24 * 2;
+
+app.get('/app.6c18889599d10afbc75c.js', function(req, res) {
+  res.set({
+    'content-encoding': 'gzip'
+  })
+  res.sendFile(path.join(__dirname, 'dist/app.6c18889599d10afbc75c.js.gz'))
+})
+
+app.get('/vendor.6c18889599d10afbc75c.js', function(req, res) {
+  res.set({
+    'content-encoding': 'gzip'
+  })
+  res.sendFile(path.join(__dirname, 'dist/vendor.6c18889599d10afbc75c.js.gz'))
+})
+
 app.use(express.static(path.join('./dist')));
 
 app.get('*', function(req, res) {
